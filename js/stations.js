a1f7eda6fde49bb794c4780210f0e0900a93e091
eastjun <eastjun@woowahan.com>
2020-04-09 09:20:23 +0900

    refactor(responsive): update responsive style

--- js/stations.js
+++ js/stations.js
@@ -1,39 +1,39 @@
-import { EVENT_TYPE } from '../utils/constants.js'
-import { listItemTemplate, initNavigation } from '../utils/templates.js'
+import { EVENT_TYPE } from "../utils/constants.js";
+import { listItemTemplate, initNavigation } from "../utils/templates.js";
 
 function StationsApp() {
-  const $stationAddButton = document.querySelector('#station-add-btn')
-  const $stationList = document.querySelector('#station-list')
+  const $stationAddButton = document.querySelector("#station-add-btn");
+  const $stationList = document.querySelector("#station-list");
 
   const onAddStationHandler = event => {
-    event.preventDefault()
-    const $stationNameInput = document.querySelector('#station-name')
-    const stationName = $stationNameInput.value
+    event.preventDefault();
+    const $stationNameInput = document.querySelector("#station-name");
+    const stationName = $stationNameInput.value;
     if (!stationName) {
-      return
+      return;
     }
-    $stationNameInput.value = ''
-    $stationList.insertAdjacentHTML('beforeend', listItemTemplate(stationName))
-  }
+    $stationNameInput.value = "";
+    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
+  };
 
   const onRemoveStationHandler = event => {
-    const $target = event.target
-    const isDeleteButton = $target.classList.contains('mdi-delete')
+    const $target = event.target;
+    const isDeleteButton = $target.classList.contains("mdi-delete");
     if (isDeleteButton) {
-      $target.closest('.station-list-item').remove()
+      $target.closest(".list-item").remove();
     }
-  }
+  };
 
   const initEventListeners = () => {
-    $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler)
-    $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler)
-  }
+    $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
+    $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+  };
 
   this.init = () => {
-    initNavigation()
-    initEventListeners()
-  }
+    initNavigation();
+    initEventListeners();
+  };
 }
 
-const stations = new StationsApp()
-stations.init()
+const stations = new StationsApp();
+stations.init();
