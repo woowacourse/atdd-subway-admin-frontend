4a8a486cbbafcebfcd65567b9b4e2f2f8fdbc5b0
임동준 <eastjun@woowahan.com>
2020-04-09 10:43:23 +0900

    style/responsive (#2)
    * refactor(app.js): separate navigation load to app.js
    
    * refactor(edges.js): remove unused function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,14 @@
-import { EVENT_TYPE } from '../utils/constants.js'
-import { stationListTemplate } from '../utils/templates.js'
+import { initNavigation } from "../utils/templates.js";
 
-function App() {
-  const $stationAddButton = document.querySelector('#station-add-btn')
-  $stationAddButton.addEventListener(EVENT_TYPE.CLICK, event => {
-    event.preventDefault()
-    const $stationNameInput = document.querySelector('#station-name')
-    const stationName = $stationNameInput.value
-    $stationNameInput.value = ''
-    document.querySelector('#station-list').insertAdjacentHTML('beforeend', stationListTemplate(stationName))
-  })
+function SubwayApp() {
+  const init = () => {
+    initNavigation();
+  };
 
-  document.querySelector('#station-list').addEventListener(EVENT_TYPE.CLICK, event => {
-    event.target.closest('.station-list-item').remove()
-  })
+  return {
+    init
+  };
 }
 
-new App()
+const subwayApp = new SubwayApp();
+subwayApp.init();
--- js/lines.js
+++ js/lines.js
@@ -1,47 +1,55 @@
-import { EVENT_TYPE } from '../utils/constants.js'
-import { listItemTemplate, initNavigation } from '../utils/templates.js'
-import { defulatSubwayLines } from '../utils/subwayMockData.js'
+import { EVENT_TYPE } from "../utils/constants.js";
+import { listItemTemplate, initNavigation } from "../utils/templates.js";
+import { defulatSubwayLines } from "../utils/subwayMockData.js";
 
 function SubwayLineApp() {
-  const $subwayLineList = document.querySelector('#subway-line-list')
-  const $stationAddButton = document.querySelector('#subway-line-add-btn')
+  const $subwayLineList = document.querySelector("#subway-line-list");
+  const $stationAddButton = document.querySelector("#subway-line-add-btn");
 
   const onAddSubwayLineHandler = event => {
-    event.preventDefault()
-    const $subwayLineNameInput = document.querySelector('#subway-line-name')
-    const subwayLineName = $subwayLineNameInput.value
+    event.preventDefault();
+    const $subwayLineNameInput = document.querySelector("#subway-line-name");
+    const subwayLineName = $subwayLineNameInput.value;
     if (!subwayLineName) {
-      return
+      return;
     }
-    $subwayLineNameInput.value = ''
-    $subwayLineList.insertAdjacentHTML('beforeend', listItemTemplate(subwayLineName))
-  }
+    $subwayLineNameInput.value = "";
+    $subwayLineList.insertAdjacentHTML(
+      "beforeend",
+      listItemTemplate(subwayLineName)
+    );
+  };
 
   const onRemoveStationHandler = event => {
-    const $target = event.target
-    const isDeleteButton = $target.classList.contains('mdi-delete')
+    const $target = event.target;
+    const isDeleteButton = $target.classList.contains("mdi-delete");
     if (isDeleteButton) {
-      $target.closest('.list-item').remove()
+      $target.closest(".list-item").remove();
     }
-  }
+  };
 
   const initDefaultSubwayLines = () => {
     defulatSubwayLines.map(line => {
-      $subwayLineList.insertAdjacentHTML('beforeend', listItemTemplate(line.title))
-    })
-  }
+      $subwayLineList.insertAdjacentHTML(
+        "beforeend",
+        listItemTemplate(line.title)
+      );
+    });
+  };
 
   const initEventListeners = () => {
-    $subwayLineList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler)
-    $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddSubwayLineHandler)
-  }
+    $subwayLineList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    $stationAddButton.addEventListener(
+      EVENT_TYPE.CLICK,
+      onAddSubwayLineHandler
+    );
+  };
 
   this.init = () => {
-    initNavigation()
-    initDefaultSubwayLines()
-    initEventListeners()
-  }
+    initDefaultSubwayLines();
+    initEventListeners();
+  };
 }
 
-const lineApp = new SubwayLineApp()
-lineApp.init()
+const lineApp = new SubwayLineApp();
+lineApp.init();
