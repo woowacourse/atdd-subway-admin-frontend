f1f907941ebc8f873a701c513db08d90f36ad8ce
BEOMJUN KIM <skyblue300a@gmail.com>
2020-04-29 15:23:06 +0900

    feat: 추가 버튼을 눌러 지하철 역을 추가할 수 있게 변경

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -4,9 +4,10 @@ import { listItemTemplate } from "../../utils/templates.js";
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $stationAddButton = document.querySelector("#station-add-btn")
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK) {
       return;
     }
     event.preventDefault();
@@ -31,6 +32,7 @@ function AdminStation() {
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
   };
 
   const init = () => {
