17f58a133b581445ac85f7a0f2ccd4bd2ee56252
BEOMJUN KIM <skyblue300a@gmail.com>
2020-04-29 15:48:35 +0900

    feat: 지하철 역 유효성 검사 추가

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -1,10 +1,12 @@
-import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
-import { listItemTemplate } from "../../utils/templates.js";
+import {EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE} from "../../utils/constants.js";
+import {listItemTemplate} from "../../utils/templates.js";
 
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
-  const $stationAddButton = document.querySelector("#station-add-btn")
+  const $stationAddButton = document.querySelector("#station-add-btn");
+
+  let stationNames = [];
 
   const onAddStationHandler = event => {
     if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK) {
@@ -13,13 +15,34 @@ function AdminStation() {
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
+    $stationNameInput.value = "";
+    const isInvalid = isStationNameInvalid(stationName);
+    if (isInvalid) {
       return;
     }
-    $stationNameInput.value = "";
+    stationNames = [...stationNames, stationName];
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
+
+  const isStationNameInvalid = name => {
+    if (!name) {
+      alert(ERROR_MESSAGE.NOT_EMPTY);
+      return true;
+    }
+    if (name.includes(" ")) {
+      alert(ERROR_MESSAGE.NOT_CONTAIN_SPACE);
+      return true;
+    }
+    if (/\d/.test(name)) {
+      alert(ERROR_MESSAGE.NOT_CONTAIN_NUMBER);
+      return true;
+    }
+    if (stationNames.includes(name)) {
+      alert(ERROR_MESSAGE.DUPLICATE_NAME);
+      return true;
+    }
+    return false;
+  }
 
   const onRemoveStationHandler = event => {
     const $target = event.target;
--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  NOT_CONTAIN_SPACE: "공백이 포함될 수 없습니다",
+  NOT_CONTAIN_NUMBER: "숫자가 포함될 수 없습니다",
+  DUPLICATE_NAME: "중복되는 역 이름은 허용되지 않습니다",
 };
 
 export const KEY_TYPE = {
