7fe7acdb1da7f4d11dd59409e648b5bc95386930
Woonjang Ahn <woonjangahn@gmail.com>
2020-04-29 15:26:07 +0900

    feat : 지하철역 검증 로직 작성

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -2,11 +2,13 @@ import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
 import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
+  let stations = [];
   const $stationInput = document.querySelector("#station-name");
+  const $stationAddBtn = document.querySelector("#station-add-btn");
   const $stationList = document.querySelector("#station-list");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.type !== EVENT_TYPE.CLICK && event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
@@ -16,6 +18,15 @@ function AdminStation() {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+    if (!/^[^\d\s]+$/.test(stationName)) {
+      alert(ERROR_MESSAGE.INVALID_INPUT);
+      return;
+    }
+    if (stations.includes(stationName)) {
+      alert(ERROR_MESSAGE.DUPLICATE);
+      return;
+    }
+    stations = [...stations, stationName];
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
@@ -23,13 +34,16 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+    const stationName = $target.closest(".list-item").innerText;
+    if (isDeleteButton && confirm("정말 삭제하시겠습니까?")) {
       $target.closest(".list-item").remove();
+      stations = stations.filter(station => station !== stationName);
     }
   };
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $stationAddBtn.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
