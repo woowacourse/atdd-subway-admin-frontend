764c54a7bc35ca3448f87d6204b44a663589e8b9
taejinseok <stj930@gmail.com>
2020-05-01 23:52:54 +0900

    feat : 역 관리 페이지 내 역 추가, 검증, 삭제 전 확인 기능

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -1,19 +1,47 @@
-import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
-import { listItemTemplate } from "../../utils/templates.js";
+import {CONFIRM_MESSAGE, ERROR_MESSAGE, EVENT_TYPE, KEY_TYPE, REGEX_PATTERN } from "../../utils/constants.js";
+import {listItemTemplate} from "../../utils/templates.js";
 
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
+  const $stationAdd = document.querySelector("#station-add-btn");
   const $stationList = document.querySelector("#station-list");
+  const isInValidStation = stationName => {
+    const invalidConditions = {
+      empty : {
+        condition : name => !name,
+        message : ERROR_MESSAGE.NOT_EMPTY
+      },
+      containsNumber : {
+        condition : name => REGEX_PATTERN.INTEGER.exec(name),
+        message : ERROR_MESSAGE.NOT_INTEGER
+      },
+      containsSpace : {
+        condition : name => REGEX_PATTERN.SPACE.exec(name),
+        message : ERROR_MESSAGE.NOT_WHITE_SPACE
+      },
+      duplicateStation : {
+        condition : name => $stationList.innerText.split("\n").includes(name),
+        message : ERROR_MESSAGE.ALREADY_EXIST_STATION
+      }
+    };
+
+    const firstInvalidObject = Object.values(invalidConditions)
+        .find(val => val.condition(stationName));
+
+    if (firstInvalidObject) {
+      alert(firstInvalidObject.message);
+    }
+    return firstInvalidObject;
+  };
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.type === EVENT_TYPE.KEY_PRESS && event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
+    if (isInValidStation(stationName)) {
       return;
     }
     $stationNameInput.value = "";
@@ -23,13 +51,14 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+    if (isDeleteButton && confirm(CONFIRM_MESSAGE.DELETE)) {
       $target.closest(".list-item").remove();
     }
   };
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $stationAdd.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
