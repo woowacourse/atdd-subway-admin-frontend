7af1ba603158ab71687db3e3e44f03d77d5a679f
hongbin23 <khbasd@gmail.com>
2020-05-01 20:32:41 +0900

    feat: admin 검증 기능 및 이벤트처리 미션진행

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -5,25 +5,37 @@ function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
 
-  const onAddStationHandler = event => {
+  const onAddStationHandler = (event) => {
     if (event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
-    if (!stationName) {
+
+    if (!checkName(stationName)) {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+
+    if (checkDuplicate(stationName)) {
+      alert("중복되는 역 이름입니다.");
+      return;
+    }
+
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
 
-  const onRemoveStationHandler = event => {
+  const onRemoveStationHandler = (event) => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+
+    if(event.target && event.target.nodeName !== "SPAN") {
+      return;
+    }
+
+    if (confirm("정말 삭제하시겠습니까?")) {
       $target.closest(".list-item").remove();
     }
   };
@@ -38,8 +50,27 @@ function AdminStation() {
   };
 
   return {
-    init
+    init,
   };
+}
+
+function checkName(stationName) {
+  const regExp = /^[^\d\s]+$/;
+  return regExp.test(stationName);
+}
+
+function checkDuplicate(stationName) {
+  let flag = false;
+  const parent = document.getElementById("station-list");
+  const child = parent.childNodes;
+  child.forEach((div) => {
+    const title = div.textContent.trim();
+    if (title === stationName) {
+      flag = true;
+      return;
+    }
+  });
+  return flag;
 }
 
 const adminStation = new AdminStation();
--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,7 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값은 공백과 숫자는 사용할 수 없어요!"
 };
 
 export const KEY_TYPE = {
