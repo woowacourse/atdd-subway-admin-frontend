7b24a2b6a0e1b2fb98d6ba83f69b92687b63eb0a
hongbin23 <khbasd@gmail.com>
2020-05-01 20:59:30 +0900

    feat: 변수명 수정 및 EVENT 타입별 분기 추가

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -2,24 +2,26 @@ import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
 import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
+  const $stationButton = document.querySelector("#station-add-btn");
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
 
   const onAddStationHandler = (event) => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.type == "keypress" && event.key !== KEY_TYPE.ENTER) {
       return;
     }
+
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
 
-    if (!checkName(stationName)) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
+    if (checkName(stationName)) {
+      alert(ERROR_MESSAGE.INVALID_INPUT);
       return;
     }
 
     if (checkDuplicate(stationName)) {
-      alert("중복되는 역 이름입니다.");
+      alert(ERROR_MESSAGE.DUPLICATE);
       return;
     }
 
@@ -30,17 +32,13 @@ function AdminStation() {
   const onRemoveStationHandler = (event) => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-
-    if(event.target && event.target.nodeName !== "SPAN") {
-      return;
-    }
-
-    if (confirm("정말 삭제하시겠습니까?")) {
+    if (isDeleteButton && confirm("정말 삭제하시겠습니까?")) {
       $target.closest(".list-item").remove();
     }
   };
 
   const initEventListeners = () => {
+    $stationButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
@@ -56,7 +54,7 @@ function AdminStation() {
 
 function checkName(stationName) {
   const regExp = /^[^\d\s]+$/;
-  return regExp.test(stationName);
+  return !regExp.test(stationName);
 }
 
 function checkDuplicate(stationName) {
