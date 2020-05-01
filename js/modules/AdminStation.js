0bd0c5632b96c0ed4055496a9f74dfa7405df3de
kimjunseok <jnunseok@gmail.com>
2020-05-01 15:16:51 +0900

    Finished TODO list

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -2,11 +2,13 @@ import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
 import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
+  let saveStations = [];
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $stationAddBtn = document.querySelector("#station-add-btn");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK) {
       return;
     }
     event.preventDefault();
@@ -16,6 +18,19 @@ function AdminStation() {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+    if (saveStations.indexOf(stationName) !== -1) {
+      alert(ERROR_MESSAGE.NOT_CONTAIN)
+      return;
+    }
+    if (/\s/.test(stationName)) {
+      alert(ERROR_MESSAGE.NOT_BLANK)
+      return;
+    }
+    if (/\d/.test(stationName)) {
+      alert(ERROR_MESSAGE.NOT_NUMBER)
+      return;
+    }
+    saveStations.push(stationName)
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
@@ -23,13 +38,15 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+    if (isDeleteButton && confirm("정말 삭제할까요?")) {
+      saveStations.splice(saveStations.indexOf($target.closest(".list-item").innerText), 1)
       $target.closest(".list-item").remove();
     }
   };
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $stationAddBtn.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  NOT_CONTAIN: "🤔 이미 등록된 역이예요.",
+  NOT_BLANK: "🤔 공백이 있으면 안돼요.",
+  NOT_NUMBER: "🤔 숫자가 있으면 안돼요."
 };
 
 export const KEY_TYPE = {
