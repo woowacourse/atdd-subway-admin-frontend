e207956b2529f2251cf1afdf1aeff08a4db994eb
lowoon <sangbo0229@gmail.com>
2020-05-02 14:34:26 +0900

    feat: 기능목록 구현

--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  NOT_INCLUDE_SPACE: "공백은 이름에 포함될 수 없습니다.",
+  NOT_INCLUDE_NUMBER: "숫자는 이름에 포함될 수 없습니다.",
+  NOT_DUPLICATED: "중복된 이름이 있습니다."
 };
 
 export const KEY_TYPE = {
