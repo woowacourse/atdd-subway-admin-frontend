13d919fc61bbceead49e7545c6d357d580237419
toneyparky <qkrtmddhks95@gmail.com>
2020-05-02 09:11:34 +0900

    feat: 추가 버튼으로 지하철역 추가하는 기능 구현

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -3,10 +3,11 @@ import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
+  const $stationAddBtn = document.querySelector("#station-add-btn");
   const $stationList = document.querySelector("#station-list");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK ) {
       return;
     }
     event.preventDefault();
@@ -30,6 +31,7 @@ function AdminStation() {
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $stationAddBtn.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
