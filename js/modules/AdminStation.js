5b447c77f0c1eabfd9bb0b5cabea3266f84b1b9c
Andy H. Jung <commaniakr@gmail.com>
2020-05-02 15:24:10 +0900

    feat: '추가' 버튼으로도 역 추가가 가능하도록 함

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -1,37 +1,17 @@
 import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE, COMFIRM_MESSAGE} from "../../utils/constants.js";
 import { listItemTemplate } from "../../utils/templates.js";
 
+
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+
   const onAddStationHandler = event => {
     if (event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
-    const $stationNameInput = document.querySelector("#station-name");
-    const stationName = $stationNameInput.value;
-    const blank_pattern = /[\s]/g;
-    const number_pattern = /[0-9]/g;
-
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
-      return;
-    }
-    if( blank_pattern.test(stationName) == true){
-      alert(ERROR_MESSAGE.SPACE_INCLUDED);
-      return;
-    }
-    if( number_pattern.test(stationName) == true){
-      alert(ERROR_MESSAGE.NUMBER_INCLUDED);
-      return;
-    }
-    if($stationList.innerText.split("\n").includes(stationName)) {
-      alert(ERROR_MESSAGE.DUPLICATE_STATION_NAME);
-      return;
-    }
-    $stationNameInput.value = "";
-    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
+    addStation();
   };
 
   const onRemoveStationHandler = event => {
@@ -42,8 +22,35 @@ function AdminStation() {
     }
   };
 
+  const addStation = () => {
+    const $stationNameInput = document.querySelector("#station-name");
+    const stationName = $stationNameInput.value;
+    const blank_pattern = /[\s]/g;
+    const number_pattern = /[0-9]/g;
+
+    if (!stationName) {
+      alert(ERROR_MESSAGE.NOT_EMPTY);
+      return;
+    }
+    if (blank_pattern.test(stationName)) {
+      alert(ERROR_MESSAGE.SPACE_INCLUDED);
+      return;
+    }
+    if (number_pattern.test(stationName)) {
+      alert(ERROR_MESSAGE.NUMBER_INCLUDED);
+      return;
+    }
+    if ($stationList.innerText.split("\n").includes(stationName)) {
+      alert(ERROR_MESSAGE.DUPLICATE_STATION_NAME);
+      return;
+    }
+    $stationNameInput.value = "";
+    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
+  }
+
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    document.querySelector("#station-add-btn").addEventListener("click", addStation);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
