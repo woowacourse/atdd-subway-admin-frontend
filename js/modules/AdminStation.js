2a6f0083fb888d250aca1aa1a96c4620877a88d6
moonyoungCHAE <xpf_fl@naver.com>
2020-05-02 23:18:33 +0900

    feat: 지하철 front-end 미션

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -4,9 +4,11 @@ import { listItemTemplate } from "../../utils/templates.js";
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $addButton = document.querySelector("#station-add-btn");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+
+    if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK) {
       return;
     }
     event.preventDefault();
@@ -16,6 +18,21 @@ function AdminStation() {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+    if (stationName.includes(" ")) {
+      alert(ERROR_MESSAGE.NOT_BLANK);
+      return;
+    }
+    const nums = /[0-9]/;
+    if (stationName.search(nums) >= 0) {
+      alert(ERROR_MESSAGE.NOT_NUMBER);
+      return;
+    }
+
+    console.log($stationList.textContent.search(stationName));
+    if ($stationList.textContent.search(stationName) >= 0) {
+      alert(ERROR_MESSAGE.NOT_DUPLICATE);
+      return;
+    }
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
@@ -23,13 +40,14 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+    if (isDeleteButton && confirm("정말로 삭제하시겠습니까?")) {
       $target.closest(".list-item").remove();
     }
   };
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $addButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
