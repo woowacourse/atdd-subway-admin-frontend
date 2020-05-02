e809d47e0f33e7a4eacc08567b657736f71ba630
KS-KIM <nullable@kakao.com>
2020-05-02 20:08:20 +0900

    feat: 지하철 추가 및 제거 기능 추가
    * 지하철 추가시 유효성 검사기능 추가
        * 공백이 포함된 경우
        * 중복되는 지하철명이 입력된 경우
        * 숫자가 포함된 경우
    * 지하철 삭제시 경고 메시지 출력

--- utils/constants.js
+++ utils/constants.js
@@ -3,10 +3,18 @@ export const EVENT_TYPE = {
   KEY_PRESS: "keypress"
 };
 
+export const WARNING_MESSAGE = {
+  STATION_DELETE_CONFIRM: "정말로 삭제하시겠습니까?"
+};
+
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  DUPLICATED: "😩 동일한 이름의 역이 존재합니다. 다시 입력해주세요. 다시 입력해주세요.",
+  NOT_ALLOWED_CHARACTER: "😡 허용되지 않은 문자열(공백 또는 숫자)이 입력되었습니다. 다시 입력해주세요."
 };
 
 export const KEY_TYPE = {
   ENTER: "Enter"
 };
+
+export const STATION_NAME_PATTERN = new RegExp(/^[^\d\s]+$/);
