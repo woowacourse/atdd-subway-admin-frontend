91136bc6f1946b475335b652470a16de3fb5157d
dd <dqrd123@gmail.com>
2020-05-03 14:31:30 +0900

    feat: 실습 반영
    - 지하철 역 명 유효성 검증
    - 지하철 역 삭제시 에러 메세지 출력
    - 추가 버튼을 통해 추가 기능 구현

--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,9 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  CONTAIN_NUMBER: "😂 숫자는 입력할 수 없습니다.",
+  DUPLICATE_NAME: "😂 역 이름이 중복 되었습니다."
 };
 
 export const KEY_TYPE = {
