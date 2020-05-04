fd1a361fac2784e35568b96b36c3b14172031867
soring <jnsorn@gmail.com>
2020-05-04 19:08:10 +0900

    feat : 지하철 역 추가 버튼 클릭 시, 지하철 역이 추가된다.

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -2,44 +2,57 @@ import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
 import { listItemTemplate } from "../../utils/templates.js";
 
 function AdminStation() {
-  const $stationInput = document.querySelector("#station-name");
-  const $stationList = document.querySelector("#station-list");
+    const $stationInput = document.querySelector("#station-name");
+    const $stationList = document.querySelector("#station-list");
+    const $stationAdd = document.querySelector("#station-add-btn");
 
-  const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
-      return;
+    const onAddStationByEnter = event => {
+        if (event.key !== KEY_TYPE.ENTER) {
+            return;
+        }
+        onAddStationHandler(event);
     }
-    event.preventDefault();
-    const $stationNameInput = document.querySelector("#station-name");
-    const stationName = $stationNameInput.value;
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
-      return;
+
+    const onAddStationByClick = event => {
+        if (event.key !== KEY_TYPE.CLICK) {
+            return;
+        }
+        onAddStationHandler(event);
     }
-    $stationNameInput.value = "";
-    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
-  };
 
-  const onRemoveStationHandler = event => {
-    const $target = event.target;
-    const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
-      $target.closest(".list-item").remove();
-    }
-  };
+    const onAddStationHandler = event => {
+        event.preventDefault();
+        const $stationNameInput = document.querySelector("#station-name");
+        const stationName = $stationNameInput.value;
+        if (!stationName) {
+            alert(ERROR_MESSAGE.NOT_EMPTY);
+            return;
+        }
+        $stationNameInput.value = "";
+        $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
+    };
 
-  const initEventListeners = () => {
-    $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
-    $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
-  };
+    const onRemoveStationHandler = event => {
+        const $target = event.target;
+        const isDeleteButton = $target.classList.contains("mdi-delete");
+        if (isDeleteButton) {
+            $target.closest(".list-item").remove();
+        }
+    };
 
-  const init = () => {
-    initEventListeners();
-  };
+    const initEventListeners = () => {
+        $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationByEnter);
+        $stationAdd.addEventListener(EVENT_TYPE.CLICK, onAddStationByClick);
+        $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    };
 
-  return {
-    init
-  };
+    const init = () => {
+        initEventListeners();
+    };
+
+    return {
+        init
+    };
 }
 
 const adminStation = new AdminStation();
