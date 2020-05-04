f2e37995c07ba1fe5e3d8e9d75dffb97163fc144
junk-sound <wom2277@naver.com>
2020-05-04 23:58:19 +0900

    feat: 마우스로 클릭해도 추가 가능

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -4,9 +4,10 @@ import { listItemTemplate } from "../../utils/templates.js";
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $stationAddButton = document.getElementById("station-add-btn");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.key !== KEY_TYPE.ENTER && event.type !== EVENT_TYPE.CLICK) {
       return;
     }
     event.preventDefault();
@@ -56,6 +57,7 @@ function AdminStation() {
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
