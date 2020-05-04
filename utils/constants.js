4e0195ae29ea34fdee61092dd102837a7d8603bd
yeonnseok <ljinsk3@gmail.com>
2020-05-04 09:13:18 +0900

    feat: 지하철 이름 유효성 검증

--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  NOT_INCLUDE_SPACE: "🤔 지하철 역 이름에는 공백이 포함될 수 없습니다.",
+  NOT_HAVE_NUMBER: "🤔 지하철 역 이름에는 숫자가 포함될 수 없습니다.",
+  NOT_DUPLICATED: "🤔 지하철 역 이름은 중복될 수 없습니다."
 };
 
 export const KEY_TYPE = {
