e724c5a802cfa863d22a3964955a94d8b76060c2
Ramen6315 <ramen6315@gmail.com>
2020-05-05 20:49:55 +0900

    feat : 유효성 검사 추가  - 공백, 숫자, 같은 역이름 검사 추가  - 상황별 에러 메세지 추가

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -5,6 +5,7 @@ function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
   const $stationClickInput = document.querySelector("#station-add-btn")
+  const number_pattern = /[0-9]/g;
 
   const onAddStationHandler = event => {
     if (event.key !== KEY_TYPE.ENTER && event.type !== 'click') {
@@ -16,7 +17,17 @@ function AdminStation() {
     if (!stationName) {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
+    } else if(number_pattern.test(stationName)) {
+      alert(ERROR_MESSAGE.NUMBER);
+      return;
+    } else if(stationName.includes(" ")) {
+      alert(ERROR_MESSAGE.BLANK);
+      return;
+    } else if($stationList.innerText.split("\n").includes(stationName)) {
+      alert(ERROR_MESSAGE.SAME);
+      return;
     }
+
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  SAME: "이미 등록된 지하철 역입니다.",
+  BLANK: "공백이 포함 되어있습니다",
+  NUMBER: "숫자는 입력 할 수 없습니다."
 };
 
 export const KEY_TYPE = {
