82d22ce92187dd6754284a30c5bf29d018511119
hyochan-kim <summacum94@naver.com>
2020-05-05 15:08:55 +0900

    [feat] 유효성 검사 / 역 추가 기능 / 삭제시 확인 기능 추가

--- utils/constants.js
+++ utils/constants.js
@@ -4,7 +4,10 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "🤔 값을 입력해주세요",
+  NOT_HAS_BLACK: "🤔 공백이 포함될 수 없어요",
+  NOT_HAS_NUMBER: "🤔 숫자가 포함될 수 없어요",
+  NOT_DUPLICATE_NAME: "🤔 동일한 이름은 안돼요"
 };
 
 export const KEY_TYPE = {
