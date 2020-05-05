55d1bd6877abd164d3303e2e602693e4a814f07c
Tak Hyungmin <milkho11@naver.com>
2020-05-05 14:38:41 +0900

    [feat] 버튼 클릭으로 지하철역 추가

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -1,68 +1,79 @@
-import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE, CONFIRM_MESSAGE } from "../../utils/constants.js";
-import { listItemTemplate } from "../../utils/templates.js";
+import {EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE, CONFIRM_MESSAGE} from "../../utils/constants.js";
+import {listItemTemplate} from "../../utils/templates.js";
 
 function AdminStation() {
-  const $stationInput = document.querySelector("#station-name");
-  const $stationList = document.querySelector("#station-list");
+    const $stationInput = document.querySelector("#station-name");
+    const $stationList = document.querySelector("#station-list");
+    const $stationAddBtn = document.querySelector("#station-add-btn");
 
-  const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
-      return;
+    const onAddStationHandler = event => {
+        let isNotEnterKeyPress = event.key !== KEY_TYPE.ENTER;
+
+        if (isNotEnterKeyPress) {
+            return;
+        }
+
+        addStation(event);
+    };
+
+    const addStation = (event) => {
+        event.preventDefault();
+
+        const $stationNameInput = document.querySelector("#station-name");
+        const stationName = $stationNameInput.value;
+
+        if (!stationName) {
+            alert(ERROR_MESSAGE.NOT_EMPTY);
+            return;
+        }
+
+        const items = document.querySelectorAll('.list-item');
+        for (let i = 0; i < items.length; i++) {
+            if (stationName === items.item(i).innerText) {
+                alert(ERROR_MESSAGE.EXIST_DUPLICATION_STATION_NAME);
+                return;
+            }
+        }
+
+        const existNumberRegExp = new RegExp("\\w*\\d\\w*");
+        if (existNumberRegExp.test(stationName)) {
+            alert(ERROR_MESSAGE.EXIST_NUMBER);
+            return;
+        }
+
+        if (stationName.indexOf(" ") !== -1) {
+            alert(ERROR_MESSAGE.EXIST_SPACE)
+            return;
+        }
+
+        $stationNameInput.value = "";
+        $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
     }
-    event.preventDefault();
-    const $stationNameInput = document.querySelector("#station-name");
-    const stationName = $stationNameInput.value;
-    if (!stationName) {
-      alert(ERROR_MESSAGE.NOT_EMPTY);
-      return;
-    }
-    const items = document.querySelectorAll('.list-item');
 
-    for(let i = 0; i < items.length; i++) {
-      if(stationName === items.item(i).innerText) {
-        alert(ERROR_MESSAGE.EXIST_DUPLICATION_STATION_NAME);
-        return;
-      }
-    }
+    const onRemoveStationHandler = event => {
+        const $target = event.target;
+        const isDeleteButton = $target.classList.contains("mdi-delete");
+        if (isDeleteButton) {
+            let deleteResponse = confirm(CONFIRM_MESSAGE.DELETE_STATION);
+            if (deleteResponse) {
+                $target.closest(".list-item").remove();
+            }
+        }
+    };
 
-    const existNumberRegExp = new RegExp("\\w*\\d\\w*");
-    if(existNumberRegExp.test(stationName)) {
-      alert(ERROR_MESSAGE.EXIST_NUMBER);
-      return;
-    }
+    const initEventListeners = () => {
+        $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+        $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+        $stationAddBtn.addEventListener(EVENT_TYPE.CLICK, addStation)
+    };
 
-    if (stationName.indexOf(" ") !== -1) {
-      alert(ERROR_MESSAGE.EXIST_SPACE)
-      return;
-    }
+    const init = () => {
+        initEventListeners();
+    };
 
-    $stationNameInput.value = "";
-    $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
-  };
-
-  const onRemoveStationHandler = event => {
-    const $target = event.target;
-    const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
-      let deleteResponse = confirm(CONFIRM_MESSAGE.DELETE_STATION);
-      if (deleteResponse) {
-        $target.closest(".list-item").remove();
-      }
-    }
-  };
-
-  const initEventListeners = () => {
-    $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
-    $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
-  };
-
-  const init = () => {
-    initEventListeners();
-  };
-
-  return {
-    init
-  };
+    return {
+        init
+    };
 }
 
 const adminStation = new AdminStation();
