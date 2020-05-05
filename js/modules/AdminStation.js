31c684f8abcccfaba9e011a4e456ad6e3f101503
icyMojito <dev.icymojito@gmail.com>
2020-05-05 19:28:04 +0900

    feat : 미션 요구사항 구현
    지하철 역 추가, 유효성 검사, 삭제 기능 추가

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -1,21 +1,43 @@
-import { EVENT_TYPE, ERROR_MESSAGE, KEY_TYPE } from "../../utils/constants.js";
-import { listItemTemplate } from "../../utils/templates.js";
+import {DELETE_MESSAGE, ERROR_MESSAGE, EVENT_TYPE, KEY_TYPE} from "../../utils/constants.js";
+import {listItemTemplate} from "../../utils/templates.js";
 
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $stationAddBtn = document.querySelector("#station-add-btn");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.type !== EVENT_TYPE.CLICK && event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
+
     if (!stationName) {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+
+    if (Array.from(stationName).some(c => c === " ")) {
+      alert(ERROR_MESSAGE.BLANK_EXIST);
+      return;
+    }
+
+    const isNumber = /[0-9]/g;
+    if (Array.from(stationName).some(c => isNumber.test(c))) {
+      alert(ERROR_MESSAGE.NUMBER_EXIST);
+      return;
+    }
+
+    const names = $stationList.getElementsByTagName("div");
+    for (let i = 0; i < names.length; i++) {
+      if (names[i].innerText === stationName) {
+        alert(ERROR_MESSAGE.DUPLICATION);
+        return;
+      }
+    }
+
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
@@ -23,7 +45,8 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+    const agree = confirm(DELETE_MESSAGE.WARNING);
+    if (isDeleteButton && agree) {
       $target.closest(".list-item").remove();
     }
   };
@@ -31,6 +54,7 @@ function AdminStation() {
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
+    $stationAddBtn.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
   };
 
   const init = () => {
--- utils/constants.js
+++ utils/constants.js
@@ -4,9 +4,16 @@ export const EVENT_TYPE = {
 };
 
 export const ERROR_MESSAGE = {
-  NOT_EMPTY: "🤔 값을 입력해주세요"
+  NOT_EMPTY: "값을 입력해주세요 😀",
+  BLANK_EXIST: "공백이 들어가면 안됩니다 😀",
+  NUMBER_EXIST: "숫자가 들어가면 안됩니다 😀",
+  DUPLICATION: "중복된 역이 있습니다 😀"
 };
 
 export const KEY_TYPE = {
   ENTER: "Enter"
 };
+
+export const DELETE_MESSAGE = {
+  WARNING: "🚨 삭제하시겠습니까?"
+}
