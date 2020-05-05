2931bc2ccb3a5268dc0b7f57e9fc87756af91501
lxxjn0 <ljy3510@gmail.com>
2020-05-05 23:19:17 +0900

    feat : 지하철 역에 공백이 들어가지 못하도록 처리

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -5,22 +5,25 @@ function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
 
-  const onAddStationHandler = event => {
+  const onAddStationHandler = (event) => {
     if (event.key !== KEY_TYPE.ENTER) {
       return;
     }
     event.preventDefault();
     const $stationNameInput = document.querySelector("#station-name");
     const stationName = $stationNameInput.value;
-    if (!stationName) {
+    const pattern = /\s/;
+
+    if (!stationName || pattern.test(stationName)) {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
 
-  const onRemoveStationHandler = event => {
+  const onRemoveStationHandler = (event) => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
     if (isDeleteButton) {
@@ -38,7 +41,7 @@ function AdminStation() {
   };
 
   return {
-    init
+    init,
   };
 }
 
