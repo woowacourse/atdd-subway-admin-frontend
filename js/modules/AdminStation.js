13bf26451599598c32c1c191e7c6355762ebbac5
jamie9504 <jamie9504@kakao.com>
2020-05-05 18:12:10 +0900

    [feat] 에러케이스 에러 발생하게 변경

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -5,7 +5,6 @@ function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
   const $stationAddButton = document.querySelector("#station-add-btn");
-
   const onAddStationHandler = event => {
     if (event.key !== KEY_TYPE.ENTER && event.type !== 'click') {
       return;
@@ -17,6 +16,21 @@ function AdminStation() {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+    if (stationName.search(/\s/) !== -1) {
+      alert(ERROR_MESSAGE.HAS_NOT_EMPTY);
+      return;
+    }
+    if (stationName.search(/[0-9]/) !== -1) {
+      alert(ERROR_MESSAGE.HAS_NOT_NUMBER);
+      return;
+    }
+    const $listItems = document.querySelectorAll(".list-item");
+    const $list = [];
+    $listItems.forEach(value => $list.push(value.innerText));
+    if ($list.includes(stationName)) {
+      alert(ERROR_MESSAGE.NOT_DUPLICATE);
+      return;
+    }
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
@@ -24,7 +38,7 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+    if (isDeleteButton && confirm("진짜 지울거에요?")) {
       $target.closest(".list-item").remove();
     }
   };
