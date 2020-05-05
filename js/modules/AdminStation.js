cdcfcab7d77bab2053f5d88eb198e610f0c2ccde
jamie9504 <jamie9504@kakao.com>
2020-05-05 17:42:11 +0900

    [feat] Click으로 추가 가능하게끔 변경

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -4,9 +4,10 @@ import { listItemTemplate } from "../../utils/templates.js";
 function AdminStation() {
   const $stationInput = document.querySelector("#station-name");
   const $stationList = document.querySelector("#station-list");
+  const $stationAddButton = document.querySelector("#station-add-btn");
 
   const onAddStationHandler = event => {
-    if (event.key !== KEY_TYPE.ENTER) {
+    if (event.key !== KEY_TYPE.ENTER && event.type !== 'click') {
       return;
     }
     event.preventDefault();
@@ -30,6 +31,7 @@ function AdminStation() {
 
   const initEventListeners = () => {
     $stationInput.addEventListener(EVENT_TYPE.KEY_PRESS, onAddStationHandler);
+    $stationAddButton.addEventListener(EVENT_TYPE.CLICK, onAddStationHandler);
     $stationList.addEventListener(EVENT_TYPE.CLICK, onRemoveStationHandler);
   };
 
