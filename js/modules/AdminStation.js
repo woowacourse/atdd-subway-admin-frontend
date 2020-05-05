d61f8c776429b1c11ab730fe31955e58c31ce6e9
sujin <sujinemilycho@gmail.com>
2020-05-05 12:32:39 +0900

    feat: 프론트엔드 미션 완료
    - 지하철 역 추가 시 유효성 검사
    - 에러 발생 시, alert 을 통한 알림
    - 지하철 역 삭제 시 confirm 으로 추가 확인

--- js/modules/AdminStation.js
+++ js/modules/AdminStation.js
@@ -16,6 +16,23 @@ function AdminStation() {
       alert(ERROR_MESSAGE.NOT_EMPTY);
       return;
     }
+      if (stationName.includes(" ")) {
+          alert(ERROR_MESSAGE.NOT_SUPPORTED_SPACE);
+          return;
+      }
+      for (let char of stationName) {
+          if ('0' <= char && char <= '9') {
+              alert(ERROR_MESSAGE.NOT_SUPPORTED_NUMBER);
+              return;
+          }
+      }
+      const $stations = $stationList.querySelectorAll(".list-item");
+      for (let station of $stations) {
+          if (station.innerText === stationName) {
+              alert(ERROR_MESSAGE.NOT_DUPLICATE);
+              return;
+          }
+      }
     $stationNameInput.value = "";
     $stationList.insertAdjacentHTML("beforeend", listItemTemplate(stationName));
   };
@@ -23,7 +40,7 @@ function AdminStation() {
   const onRemoveStationHandler = event => {
     const $target = event.target;
     const isDeleteButton = $target.classList.contains("mdi-delete");
-    if (isDeleteButton) {
+      if (isDeleteButton && confirm("정말 삭제하시겠습니까?")) {
       $target.closest(".list-item").remove();
     }
   };
